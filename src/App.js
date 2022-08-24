267f0b9e757c65a489668f733ac04113c22a6181
Deandre Clark <Deandreclark123@@gmail.com>
2022-08-24 12:22:57 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Overview from './components/Overview';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  
+  render(){
+    return(
+      <div> 
+        <PersonalInfo/>
+
+      </div>
+      
+    )
+  }
+}
+
+class PersonalInfo extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      first: '',
+      last: '',
+      title: '',
+      photo: '',
+      address: '',
+      phone: '',
+      email:'',
+      description:'',
+
+      info: []
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (e) =>{
+    const target = e.target;
+    const value = target.value;
+    const name = target.name
+    this.setState({
+      [name] : value,
+    })
+    
+
+    console.log(this.state.first);
+    console.log(this.state.info);
+  }
+
+  render(){
+    const { first, last, title, address, phone, description } = this.state
+    return(
+      <div>
+        <form>
+        <input type="text" id="first-name" placeholder='First Name' name="first" onChange={this.handleChange}/>
+        <input type="text" id="last-name" placeholder='Last Name' name="last" onChange={this.handleChange}/>
+        <input type="text" id="title" placeholder='Title' name="title" onChange={this.handleChange}/>
+        <input type="text" id="photo" placeholder='Photo' name="photo" onChange={this.handleChange}/>
+        <input type="text" id="address" placeholder='Address' name="address" onChange={this.handleChange}/>
+        <input type="text" id="phone" placeholder='Phone' name="phone" onChange={this.handleChange}/>
+        <input type="text" id="email" placeholder='Email' name="email" onChange={this.handleChange}/>
+        <input type="text" id="description" placeholder='descrption' name="description" onChange={this.handleChange}/>
+        <button id="add">Add</button>
+        <button id="delete">Delete</button>
+      </form>
+      <Overview first={first} last={last} title={title} address={address} phone={phone} description={description} />
+      </div>
+      
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Overview.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Overview = (props) =>{
+    const {first, last, title, address, phone, description} = props;
+
+    return (
+        <div className='personal-info'></div>
+    )
+}
+
+export default Overview;
